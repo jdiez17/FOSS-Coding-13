d4390e3b87b2131d4c6c5f1c990178e1db6e4038
jdiez17 <jose.manuel.diez@gmail.com>
2013-07-27 13:20:41 +0200

    Basic game working

--- maze2013/views.py
+++ maze2013/views.py
@@ -6,11 +6,11 @@ from .maze import maze
 
 class MazeView(FlaskView):
     def index(self):
-        return render_template("maze.html")
+        return render_template("maze.html", player=2, level=6)
 
     @json_output
     def get(self, complexity):
         x = y = 299 / float(complexity)
         return {
-            'maze': maze(int(x), int(y), None),
+            'maze': maze(int(x), int(y)),
         }
--- static/app.js
+++ static/app.js
@@ -1,7 +1,13 @@
 var ctx;
 var maze;
+var player_coords;
+var x_max = y_max = 300;
 
-function draw_maze(maze, cube_dim) {
+function get_fill_style(num) {
+    return "red";
+}
+
+function draw_maze() {
     ctx.clearRect(0, 0, 300, 300);
     for(y = 0; y < maze.length; y++) {
         for(x = 0; x < maze[y].length; x++) {
@@ -16,17 +22,65 @@ function draw_maze(maze, cube_dim) {
                 case 2:
                     ctx.fillStyle = "yellow";
                     break;
+                default:
+                    ctx.fillStyle = get_fill_style(num);
+                    if(num == player) {
+                        player_coords = [x, y];
+                    }
+                    break;
             }
-            ctx.fillRect(x * cube_dim, y * cube_dim, cube_dim, cube_dim);
+            ctx.fillRect(x * level, y * level, level, level);
         }
     }
 }
 
-function load_maze(complexity) {
-    $.get("/maze/" + complexity, function(data) {
+function load_maze(req) {
+    $.get("/maze/" + req, function(data) {
         maze = data['maze'];
-        draw_maze(maze, complexity);
+        level = req;
+        draw_maze();
     });
+}
+
+function valid_move(x, y) {
+    if(x_max >= x && y_max >= y && x >= 0 && y >= 0) {
+        if(maze[y][x] == 0 || maze[y][x] == 2)
+            return true;
+    }
+    return false;
+}
+
+function keyhandler(ev) {
+    var c = String.fromCharCode(ev.which);
+    var dx, dy;
+    if(c == "w") {
+        dx = 0;
+        dy = -1;
+    }
+    if(c == "a") {
+        dx = -1;
+        dy = 0;
+    }
+    if(c == "s") {
+        dx = 0;
+        dy = 1;
+    }
+    if(c == "d") {
+        dx = 1;
+        dy = 0;
+    }
+    
+    var x = player_coords[0] + dx;
+    var y = player_coords[1] + dy;
+    if(maze[y][x] == 2) {
+        alert("You win");
+    }
+    if(valid_move(x, y)) {
+        maze[player_coords[1]][player_coords[0]] = 0;
+        maze[y][x] = player;
+        draw_maze();
+    }
+
 }
 
 $(document).ready(function() {
@@ -34,5 +88,6 @@ $(document).ready(function() {
     console.log(canvas);
     ctx = canvas.getContext("2d");
 
-    load_maze(2);    
+    load_maze(level);
+    $(document).keypress(keyhandler);
 });
